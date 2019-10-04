1d6fca20f9dbfe58a2e0637c9094941eb0e052be
ignacioguadarramaislas <ignacioguadarramaislas@gmail.com>
2019-10-04 11:27:44 -0500

    cambio en telefono del index y el localhost de la conexion a base de datos

--- Connections/igi.php
+++ Connections/igi.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_igi = "10.10.1.3";
-$database_igi = "andrea";
-$username_igi = "ignacio";
-$password_igi = "291078";
+$hostname_igi = "localhost";
+$database_igi = "servi295_andrea";
+$username_igi = "servi295_ignacio";
+$password_igi = "#antares291078igi";
 $igi = mysql_pconnect($hostname_igi, $username_igi, $password_igi) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
